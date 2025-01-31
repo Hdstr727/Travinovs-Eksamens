6de77d179676b663c99ef559973d2cecde1194ee
Hdstr727 <helldestroerr05@gmail.com>
2025-01-31 12:45:02 +0200

    First commit, added laravel

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        Task::create([
+            'title' => $request->title,
+        ]);
+
+        return redirect()->back()->with('success', 'Задача успешно добавлена!');
+    }
+}
