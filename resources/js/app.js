4ecc07d7523e6beba4654b508fa961fa2b07a07d
Hdstr727 <helldestroerr05@gmail.com>
2025-02-02 21:30:18 +0200

    Added laravel breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,27 @@
-import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import App from './components/App.vue';
-import Home from './components/Home.vue';
+import '../css/app.css';
+import './bootstrap';
 
-// Define routes
-const routes = [
-    { path: '/', component: Home },
-];
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
 });
-
-// Create Vue app
-createApp(App).use(router).mount('#app');
